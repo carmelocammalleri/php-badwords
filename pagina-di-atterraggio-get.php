e1cfc930a6dc62e4b1c74f0f55d88c26811754dd
Melozzo <carmecamma1995@gmail.com>
2023-11-01 11:27:39 +0100

    [get]: paragraph in file pagina atterraggio get

--- pagina-di-atterraggio-get.php
+++ pagina-di-atterraggio-get.php
@@ -1,7 +1,9 @@
 <?
 $badword = isset($_GET['badword']) ? $_GET['badword'] :  "NO WORDS, YOU MUST INSERT FISIC WORD ";
 
+$textParagraph = isset($_GET['text-area']) ? $_GET['text-area'] : '';
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,6 +17,7 @@ $badword = isset($_GET['badword']) ? $_GET['badword'] :  "NO WORDS, YOU MUST INS
     <div class="col">
       <h2>Atterraggio Form in GET</h2>
       <h2>Parola da censurare: <?php echo $badword ?></h2>
+      <p><?php echo $textParagraph ?></p>
     </div>
   </div>
 </div>
